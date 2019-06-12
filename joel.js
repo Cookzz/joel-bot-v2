2c838414c7279b1ee6c4ff8e36404823920e07ec
Cookzz <38864138+Cookzz@users.noreply.github.com>
2019-06-12 22:49:59 +0800

    update 2

--- joel.js
+++ joel.js
@@ -2,21 +2,12 @@ const Discord = require('discord.js')
 
 const client = new Discord.Client()
 
-<<<<<<< HEAD
 // The token of your bot - https://discordapp.com/developers/applications/me
 const token = 'NDM0Nzg1MzY5NDc3NzQyNTky.DbSlZA.A0OQGlz3Jp7WUJgr1D-NNf1P1eE'
-=======
-const token = 'NDM0Nzg1MzY5NDc3NzQyNTky.DbSlZA.A0OQGlz3Jp7WUJgr1D-NNf1P1eE';
->>>>>>> f66fe301864ffc66863dfc94cb6a378f46cee548
 
 let songList = []
 
-<<<<<<< HEAD
 const ytdl = require('ytdl-core-discord')
-=======
-const ytdl = require('ytdl-core')
-client.login(token)
->>>>>>> f66fe301864ffc66863dfc94cb6a378f46cee548
 
 client.on('ready', () => {
     console.log('joel bot ready')
@@ -24,14 +15,10 @@ client.on('ready', () => {
 
 client.on("error", function () {
     console.log("error!")
-<<<<<<< HEAD
     client.login(token)
-=======
->>>>>>> f66fe301864ffc66863dfc94cb6a378f46cee548
 });
 
 client.on('message', async message => {
-<<<<<<< HEAD
     var sender = message.member.user.tag;
     var userVoiceChannel = message.member.voiceChannel;
 
@@ -40,11 +27,6 @@ client.on('message', async message => {
             console.log("joined channel");
         });
     }
-=======
-  if(message.author.id!="434785369477742592"){
-    message.reply("working")
-  }
->>>>>>> f66fe301864ffc66863dfc94cb6a378f46cee548
 });
 
 // Log our bot in
