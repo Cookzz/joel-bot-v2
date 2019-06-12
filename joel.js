ea2c32ada68c03d4f1613697f87bad4fd3919c03
Cookzz <38864138+Cookzz@users.noreply.github.com>
2019-06-12 22:45:03 +0800

    update 2

--- joel.js
+++ joel.js
@@ -5,12 +5,12 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'NDM0Nzg1MzY5NDc3NzQyNTky.DbSlZA.A0OQGlz3Jp7WUJgr1D-NNf1P1eE';
+const token = 'NDM0Nzg1MzY5NDc3NzQyNTky.DbSlZA.A0OQGlz3Jp7WUJgr1D-NNf1P1eE'
 
 const PREFIX = "-"
 const songList = []
 
-const ytdl = require('ytdl-core')
+const ytdl = require('ytdl-core-discord')
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -20,10 +20,20 @@ client.on('ready', () => {
 
 client.on("error", function () {
     console.log("error!")
-    client.login(token	)
+    client.login(token)
 });
 
 // Create an event listener for messages
 client.on('message', async message => {
-    message.reply("working")
+    var sender = message.member.user.tag;
+    var userVoiceChannel = message.member.voiceChannel;
+
+    if (sender != "Joel#0900"){
+        userVoiceChannel.join().then(connection => {
+            console.log("joined channel");
+        });
+    }
 });
+
+// Log our bot in
+client.login(token);
