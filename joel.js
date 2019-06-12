f66fe301864ffc66863dfc94cb6a378f46cee548
LeeJiaLe <leejiale456@gmail.com>
2019-06-12 22:21:11 +0800

    edited

--- joel.js
+++ joel.js
@@ -1,29 +1,24 @@
-// Import the discord.js module
 const Discord = require('discord.js')
 
-// Create an instance of a Discord client
 const client = new Discord.Client()
 
-// The token of your bot - https://discordapp.com/developers/applications/me
 const token = 'NDM0Nzg1MzY5NDc3NzQyNTky.DbSlZA.A0OQGlz3Jp7WUJgr1D-NNf1P1eE';
 
-const PREFIX = "-"
-const songList = []
+let songList = []
 
 const ytdl = require('ytdl-core')
+client.login(token)
 
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
 client.on('ready', () => {
     console.log('joel bot ready')
 });
 
 client.on("error", function () {
     console.log("error!")
-    client.login(token	)
 });
 
-// Create an event listener for messages
 client.on('message', async message => {
+  if(message.author.id!="434785369477742592"){
     message.reply("working")
+  }
 });
