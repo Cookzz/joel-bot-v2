55b60f3a5f2862dbb16eb3a645003bbadd6d1a8f
Cookzz <38864138+Cookzz@users.noreply.github.com>
2019-06-13 00:35:42 +0800

    with skip and prefix now

--- joel.js
+++ joel.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
+const ytdl = require('ytdl-core-discord')
 
 const client = new Discord.Client()
-
 const token = 'NDM0Nzg1MzY5NDc3NzQyNTky.DbSlZA.A0OQGlz3Jp7WUJgr1D-NNf1P1eE';
+const prefix = ","
 
 let songList = []
 let d;
-const ytdl = require('ytdl-core-discord')
+
 
 client.login(token)
 
@@ -20,20 +21,34 @@ client.on("error", function () {
 
 client.on('message', async message => {
     let userVoiceChannel = message.member.voiceChannel
-    let content = message.content
+    let content = message.content.split(',')
+    let msg = content[1].split(' ')
 
-    if(message.author.id!="434785369477742592"){
-        if (songList.length==0){
-            songList.push(content)
-            userVoiceChannel.join().then(connection => {
-                console.log("joined channel");
-                play(userVoiceChannel, connection, content)
-            })
-        } else {
-            songList.push(content)
+    if (content[0] == prefix){
+        if(message.author.id!="434785369477742592"){
+            if (msg[0] == "play"){
+                if (songList.length==0){
+                    songList.push(msg[1])
+                    userVoiceChannel.join().then(connection => {
+                        console.log("joined channel");
+                        play(userVoiceChannel, connection, msg[1])
+                    })
+                } else {
+                    songList.push(msg[1])
+                }
+            }
+
+            if (msg[0] == "skip"){
+                if (songList.length > 0){
+                    skip(userVoiceChannel, connection)
+                } else {
+                    message.reply("There's no more song to skip.")
+                }
+            }
+            //message.reply("working")
         }
-
-        //message.reply("working")
+    } else {
+        message.reply("Invalid command")
     }
 });
 
@@ -48,6 +63,12 @@ async function play(userVoiceChannel, connection, url) {
             userVoiceChannel.leave()
         }
     })
+}
+
+async function skip(userVoiceChannel, connection) {
+    songList.shift()
+    console.log(songList[0])
+    play(userVoiceChannel, connection, songList[0])
 }
 // Log our bot in
 client.login(token);
