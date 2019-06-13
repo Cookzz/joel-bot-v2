06599cda844a4fdd36b44731ffa29efbd96cfd53
LeeJiaLe <leejiale456@gmail.com>
2019-06-14 01:43:59 +0800

    commit

--- joel.js
+++ joel.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const ytdl = require('ytdl-core-discord');
 const ytdlCore = require('ytdl-core');
 
 const client = new Discord.Client();
@@ -13,7 +12,6 @@ let option = {};
 let willLoop = false;
 let connected;
 let d;
-
 
 client.login(token)
 
@@ -32,22 +30,21 @@ client.on('message', async message => {
 
     if (message.content[0] == prefix){
         if(message.author.id!="434785369477742592"){
-			command(message, msg)
-			
-            //message.reply("working")
+			       command(message, msg)
         }
     }
 });
+
 
 function command(u,m){
 	let command={
 		p:(u,m)=>addMusic(u,m),
 		s:(u,m)=>skip(u,m),
-		//se:(u,m)=>seek(u,m),
+		se:(u,m)=>seek(u,m),
 		pa: (u,m)=>pause(u,m),
 		re: (u,m)=>resume(u,m),
 		l: (u,m)=>leave(u,m),
-		loop: ()=>loop(),
+		loop: (u,m)=>loop(u,m),
 	}
 
 	if(command.hasOwnProperty(m[0])){
@@ -64,11 +61,20 @@ function addMusic(u,m){
 		if (songList.length==0){
 			voice.join().then(connection => {
 				console.log("joined channel");
-				play(voice, connection, m[1])
+				play(voice, connection, {
+          url:m[1],
+          option:{}
+        })
 			})
 		}
-		songList.push(m[1])
-		allSongList.push(m[1])
+		songList.push({
+      url:m[1],
+      option:{}
+    })
+		allSongList.push({
+      url:m[1],
+      option:{}
+    })
 
 		ytdlCore.getInfo(m[1]).then((info) => {
 			u.channel.send("TITLE: " + info.title)
@@ -79,21 +85,16 @@ function addMusic(u,m){
 	}
 }
 
-function loop(){
-	if (willLoop){
-		willLoop = false;
-		u.channel.send('Loop disabled.')
-	} else {
-		willLoop = true;
-		u.channel.send('Loop enabled.')
-	}
+function loop(u,m){
+  willLoop = !willLoop
+	u.channel.send('Loop '+(willLoop?'enabled':'disabled'));
 }
 
 function skip(u,m) {
     if (songList.length > 0){
      	d.end();
     } else {
-      	u.reply("There's no more song to skip.")
+      u.reply("There's no more song to skip.")
     }
 }
 
@@ -104,9 +105,7 @@ function leave(u,m) {
 	if (botConnection){
 		voice.leave()
 		u.channel.send('Left voice channel.')
-
 		songList = []
-		allSongList = []
 	} else {
 		u.channel.send('I am not in a voice channel.')
 	}
@@ -122,48 +121,57 @@ function pause(u,m){
 }
 
 function resume(u,m){
-	if (songList.length > 0){
-		d.resume();
-		u.channel.send('Resumed.')
+	 if (songList.length > 0){
+    	d.resume();
+    	u.channel.send('Resumed.')
    } else {
-		u.reply("There's no song playing.")
+  	  u.reply("There's no song playing.")
    }
 }
 
-// function seek(u,m){
-// 	let voice=u.member.voiceChannel
-// 	let time = 0;
-// 	if (m[1]){
-// 		time = m[1]
-// 	}
+function seek(u,m){
+	let voice=u.member.voiceChannel
+	let time = 0;
+	if (m[1]){
+		time = m[1]
+	}
 
-// 	option = {
-// 		seek: time
-// 	}
+  songList.splice(1, 0, {
+    url:currentSong,
+    option:{
+  		seek: parseInt(time)
+  	}
+  });
 
-// 	play(voice, connected, currentSong)
-// }
+  d.end();
 
-async function play(userVoiceChannel, connection, url) {
-	d=connection.playOpusStream(await ytdl(url))
+}
 
-	currentSong = url
-	
-    d.on("end",end=>{
-        songList.shift()
-        if (songList.length > 0){
-            console.log(songList[0])
-            play(userVoiceChannel, connection, songList[0])
-        } else {
+async function play(userVoiceChannel, connection, song) {
+  const stream = ytdlCore(song.url, { filter : 'audioonly' });
+	d=connection.playStream(stream, song.option)
+
+  currentSong = song.url
+
+  d.on("end",end=>{
+    songList.shift()
+    if (songList.length > 0){
+        console.log(songList[0])
+        play(userVoiceChannel, connection, songList[0])
+    } else {
 			if (willLoop){
-				songList = allSongList;
+				songList = j2j(allSongList);
 				play(userVoiceChannel, connection, songList[0])
 			} else {
 				allSongList = [];
 				userVoiceChannel.leave()
 			}
-        }
-    })
+    }
+  })
+}
+
+function j2j(j){
+  return JSON.parse(JSON.stringify(j));
 }
 // Log our bot in
 client.login(token);
