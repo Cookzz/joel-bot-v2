d320d9cf7536e4c15b5850e3c657ac6a5f5cbaa9
Cookies <38864138+Cookzz@users.noreply.github.com>
2025-03-14 15:05:28 +0800

    chore: minor cleanup

--- src/utils/common.util.ts
+++ src/utils/common.util.ts
@@ -26,17 +26,17 @@ export const getDuration = (durations: string[]): string => {
 
 export const sumDurations = (durations: string[]): number => {
     return durations.reduce((sum, string) => {
-        var mins, secs;
+        let mins, secs;
         [mins, secs] = string.split(":").slice(-2).map(n => parseInt(n, 10));
         return sum + mins * 60 + secs;
     }, 0);
 }
   
 export const formatDuration = (duration: number): string => {
-    let hours = duration / 3600 | 0;
-    let minutes = duration % 3600 / 60 | 0;
-    let seconds = duration % 60;
-    let minsSecs = `${pad(minutes)}:${pad(seconds)}`;
+    const hours = duration / 3600 | 0;
+    const minutes = duration % 3600 / 60 | 0;
+    const seconds = duration % 60;
+    const minsSecs = `${pad(minutes)}:${pad(seconds)}`;
   
     return hours > 0 ? `${hours}:${minsSecs}` : `00:${minsSecs}`;
 }
